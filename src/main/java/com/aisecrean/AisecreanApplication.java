8e26bff916abda1015bd420bb32ea3c5eaf2bda8
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2026-03-15 01:43:08 -0300

    add

---
+++ src/main/java/com/aisecrean/AisecreanApplication.java
@@ -0,0 +1,13 @@
+package com.aisecrean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisecreanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisecreanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aisecrean/AisecreanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisecrean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisecreanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
